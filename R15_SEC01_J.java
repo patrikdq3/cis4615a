421e8bf014bd3f83df5e67cbd51fc762810b9215
Patrik De Quevedo <136861168+patrikdq3@users.noreply.github.com>
2025-10-04 15:08:03 -0400

    Update R15_SEC01_J.java
    Compliant Code

--- R15_SEC01_J.java
+++ R15_SEC01_J.java
@@ -1,11 +1,18 @@
 private void privilegedMethod(final String filename)
                               throws FileNotFoundException {
+  final String cleanFilename;
+  try {
+    cleanFilename = cleanAFilenameAndPath(filename);
+  } catch (/* exception as per spec of cleanAFileNameAndPath */) {
+    // Log or forward to handler as appropriate based on specification
+    // of cleanAFilenameAndPath
+  }
   try {
     FileInputStream fis =
         (FileInputStream) AccessController.doPrivileged(
           new PrivilegedExceptionAction() {
         public FileInputStream run() throws FileNotFoundException {
-          return new FileInputStream(filename);
+          return new FileInputStream(cleanFilename);
         }
       }
     );
