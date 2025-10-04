643996c289369fc2666728f9d815f3e783dfe50b
Patrik De Quevedo <136861168+patrikdq3@users.noreply.github.com>
2025-10-04 15:05:56 -0400

    Update R09_LCK02_J.java
    Compliant Code

--- R09_LCK02_J.java
+++ R09_LCK02_J.java
@@ -1,19 +1,13 @@
+
 class Base {
   static DateFormat format =
       DateFormat.getDateInstance(DateFormat.MEDIUM);
  
   public Date parse(String str) throws ParseException {
-    synchronized (getClass()) {
+    synchronized (Base.class) {
       return format.parse(str);
     }
   }
 }
  
-class Derived extends Base {
-  public Date doSomethingAndParse(String str) throws ParseException {
-    synchronized (Base.class) {
-      // ...
-      return format.parse(str);
-    }
-  }
-}
+// ...
