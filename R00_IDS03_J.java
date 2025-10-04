36c06cb53c1d1acaa216bbdff3f374de90c64272
Patrik De Quevedo <136861168+patrikdq3@users.noreply.github.com>
2025-10-04 15:01:41 -0400

    Update R00_IDS03_J.java
    Compliant Code

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,5 +1,10 @@
 if (loginSuccessful) {
-    logger.severe("User login succeeded for: " + username);
+    logger.severe("User login succeeded for: " + sanitizeUser(username));
 } else {
-    logger.severe("User login failed for: " + username);
+    logger.severe("User login failed for: " + sanitizeUser(username));
+}
+
+public String sanitizeUser(String username) {
+    return Pattern.matches("[A-Za-z0-9]+", username) 
+        ? username : "unauthorized user";
 }
